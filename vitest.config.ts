dcd4c4c0cdd30865e43b22bff97cb10c08248396
Henrique <henrique@sidekick-coder.com>
2025-06-16 18:43:10 -0300

    feat: add vitest configuration for testing with TypeScript path aliases

---
+++ vitest.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vitest/config'
+import tsconfigPaths from 'vite-tsconfig-paths'
+
+export default defineConfig({
+    test: {
+        watch: false,
+        reporters: ['verbose'],
+        exclude: ['**/node_modules/**', '**/dist/**'],
+    },
+    plugins: [tsconfigPaths()],
+})
